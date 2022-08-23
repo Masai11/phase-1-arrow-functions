204dea6a1b87e88044c9d0ddf4a9f9fe50b26a13
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-23 17:33:36 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,11 @@
+const a = 2000
+const b = 100
+const divide = function(){
+    return a / b;
+}
+
+const square = (parameter1) => parameter1 * parameter1;
+square(2,3); 
+
+const add = (para1, para2) => para1 + para2;
+add(4,6); 
